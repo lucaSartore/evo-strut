b4e51746498d231d1e74df50865dca4049fe2fb5
Luca Sartore <lucasartore02@gmail.com>
2026-02-22 11:51:36 +0100

    add settings

--- src/models/settings.rs
+++ src/models/settings.rs
@@ -2,7 +2,33 @@ use std::f32::consts::PI;
 
 #[derive(Default, Debug, Clone)]
 pub struct Settings {
-    pub bridge_settings: BridgeSettings
+    /// parameters that decide what constitute a valid bridge
+    pub bridge_settings: BridgeSettings,
+    /// parameters that define what constitute a "critical" surface
+    /// (i.e. a surface that needs supports)
+    pub criticality_settings: CriticalitySettings,
+}
+
+#[derive(Debug, Clone)]
+pub  struct CriticalitySettings {
+    /// minimum angle for which supports are added
+    /// if set to zero all overhangs will be supported
+    /// if set to 90 none of the overhangs will be supported
+    /// measured in degrees
+    pub support_overhanging_angle: f32,
+    /// max detachment from the Z plane that a surface can have
+    /// before is considered not supported.
+    /// measured in mm
+    pub max_detachment_from_z_plane: f32
+}
+
+impl Default for CriticalitySettings {
+    fn default() -> Self {
+        Self { 
+            support_overhanging_angle: 60.,
+            max_detachment_from_z_plane: 0.1
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
