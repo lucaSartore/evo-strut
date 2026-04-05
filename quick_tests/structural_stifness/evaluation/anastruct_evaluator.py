fix stiffness series calculation by intriducing rotation

quick_tests/structural_stifness/evaluation/anastruct_evaluator.py
@@ -17,31 +17,40 @@ class AnastructEvaluator(Evaluator):
     @staticmethod
     def evaluate_node(graph: Graph, node_id: NodeId, settings: Settings) -> Stiffness:
         # displacement we get by applying a force on the x axis
-        [dx_x, dy_x] = AnastructEvaluator.get_displacement(graph, node_id, settings, 1.0, 0);
+        [dx_x, dy_x, dth_x] = AnastructEvaluator.get_displacement(graph, node_id, settings, 1.0, 0.0, 0.0);
         # displacement we get by applying a force on the y axis
-        [dx_y, dy_y] = AnastructEvaluator.get_displacement(graph, node_id, settings, 0.0, 1.0);
-        m = np.asarray([[dx_x, dx_y], [dy_x, dy_y]])
+        [dx_y, dy_y, dth_y] = AnastructEvaluator.get_displacement(graph, node_id, settings, 0.0, 1.0, 0.0);
+        # displacement we get by applying a momentum (theta)
+        [dx_th, dy_th, dth_th] = AnastructEvaluator.get_displacement(graph, node_id, settings, 0.0, 0.0, 1.0);
+        m = np.asarray([
+            [dx_x, dx_y, dx_th],
+            [dy_x, dy_y, dy_th],
+            [dth_x, dth_y, dth_th],
+        ])
         if (m == 0).all():
             m = STIFFNESS_MATRIX_OF_GROUND
         else:
             m = np.linalg.inv(m)
-        # print(f"matrix for node {node_id} is {m}")
         return m
 
 
     @staticmethod
-    def get_displacement(graph: Graph, node_id: NodeId, settings: Settings, fx: float, fy: float) -> tuple[float, float]:
+    def get_displacement(graph: Graph, node_id: NodeId, settings: Settings, fx: float, fy: float, fth: float) -> tuple[float, float, float]:
         ss = AnastructEvaluator.build_simulator(graph, settings)
         id = ss.find_node_id(graph.nodes[node_id].position.as_list());
         assert id != None
-        ss.point_load(Fx=fx, Fy = fy, node_id=id)
+        if fx != 0 or fy != 0:
+            ss.point_load(Fx=fx, Fy=fy, node_id=id)
+        if fth != 0:
+            ss.moment_load(Ty=fth, node_id=id)
         displacement = ss.solve()
         dx = displacement[(id-1) * 3]
         dy = -displacement[(id-1) * 3 + 1]
-        # print(dx, dy)
+        dth = displacement[(id-1) * 3 + 2]
+        # print(dx, dy, dth)
         # ss.show_structure()
         # ss.show_displacement()
-        return (float(dx), float(dy))
+        return (float(dx), float(dy), float(dth))
 
 
     @staticmethod

quick_tests/structural_stifness/evaluation/util.py
@@ -1,21 +1,41 @@
 import numpy as np
-from functools import reduce
 from custom_types import Stiffness, Point, Settings
 import math
 
 
-# calculate the stiffness of a series of multiple trusses in series
-def stiffness_series(s: list[Stiffness]) -> Stiffness:
-    e = np.sum(np.stack([np.linalg.inv(e) for e in s]), axis=0)
-    assert e.shape == (2,2)
-    return np.linalg.inv(e)
+def stiffness_series(base_stiffness: Stiffness, point_from: Point, point_to: Point, settings: Settings) -> Stiffness:
+    beam_stiffness = calculate_beam_stiffness(point_from, point_to, settings)
+    beam_vector = point_to - point_from
+
+    # transformation matrix to project
+    conversion_matrix = np.asarray([
+        [1, 0, -beam_vector.y],
+        [0, 1, beam_vector.x],
+        [0, 0, 1]
+    ])
+
+    base_compliance = np.linalg.inv(base_stiffness)
+    beam_compliance = np.linalg.inv(beam_stiffness)
+
+    # in the second part of the equation we are:
+    # - projecting the force in the base frame (conversion_matrix.T)
+    # - calculating the displacement of the force (base_compliance)
+    # - re-projecting the displacement on the final beam (conversion_matrix)
+    # the two compliances are then summed (summing the distortion that is generated
+    # by the first stick and by the second stick)
+    full_compliance = beam_compliance + conversion_matrix @ base_compliance @ conversion_matrix.T
+
+    full_stiffness = np.linalg.inv(full_compliance)
+    return full_stiffness
+
 
 
 # calculate the stiffness of a series of multiple trusses in parallel
 def stiffness_parallel(s: list[Stiffness]) -> Stiffness:
     e = np.sum(np.stack(s), axis=0)
-    assert e.shape == (2,2)
+    assert e.shape == (3,3)
     return e
+
 
 # calculate the stiffness of a Cantilever Beam
 def calculate_beam_stiffness(point_from: Point, point_to: Point, settings: Settings) -> Stiffness:
@@ -24,24 +44,35 @@ def calculate_beam_stiffness(point_from: Point, point_to: Point, settings: Setti
 
     # first we assume the beam is horizontal
     distance = Point.distance(point_from, point_to)
-    stiffness_y = 3 * ei / (distance**3)
-    stiffness_x = ea / distance
+    kxx = ea / distance
+    kyy = 12 * ei / (distance**3)
+    kyt = -6 * ei / (distance**2)
+    kty = -6 * ei / (distance**2)
+    ktt = 4 * ei / distance
 
-    stiffness = np.asarray([[stiffness_x, 0], [0,stiffness_y]])
+    stiffness = np.asarray([
+        [kxx, 0, 0], 
+        [0, kyy, kyt],
+        [0, kty, ktt]
+    ])
 
     # return stiffness
     # the we rotate the stiffness
     diff = point_to - point_from
+    # todo: it is still unclear to me why there shall be a minus here...
     angle = -math.atan2(diff.y, diff.x)
 
     rotation_matrix = np.asarray(
-        [[math.cos(angle), -math.sin(angle)],
-         [math.sin(angle),  math.cos(angle)]]
+        [[math.cos(angle), -math.sin(angle), 0],
+         [math.sin(angle),  math.cos(angle), 0],
+         [0,                0,               1]
+         ]
     )
+
     rotated = rotation_matrix.T @ stiffness @ rotation_matrix
-    print(f"angle: {angle}")
-    print(f"pre-rotation: {stiffness}")
-    print(f"post-rotation: {rotated}")
+    # print(f"angle: {angle}")
+    # print(f"pre-rotation: {stiffness}")
+    # print(f"post-rotation: {rotated}")
 
     return rotated
 
@@ -50,8 +81,7 @@ def calculate_stiffness(point: Point, supports: list[tuple[Point, Stiffness]], s
     supports_stiffness: list[Stiffness] = []
 
     for (support_p ,support_s) in supports:
-        beam_stiffness = calculate_beam_stiffness(support_p, point, settings)
-        full_stiffness = stiffness_series([support_s, beam_stiffness])
+        full_stiffness = stiffness_series(support_s, support_p, point, settings)
         supports_stiffness.append(full_stiffness)
 
     return stiffness_parallel(supports_stiffness)

quick_tests/structural_stifness/visualize.py
@@ -45,10 +45,12 @@ class Visualizer:
                 if self.graph.nodes[node_id].ground_node:
                     continue
                 node = self.graph.nodes[node_id]
-                if stiffness.shape != (2, 2):
-                    raise ValueError("Stiffness matrix must be a 2x2 array")
 
-                matrix = np.asarray(stiffness, dtype=float)
+                if stiffness.shape != (3, 3):
+                    raise ValueError("Stiffness matrix must be a 3x3 array")
+                # we can show only displacement in x and y (can't visualize theta properly)
+                matrix = np.asarray(stiffness, dtype=float)[0:2, 0:2]
+                # we invert matrix (we want to show displacement [m/N] rather than stiffness [N/m]
                 matrix = np.linalg.inv(matrix)
                 if not np.allclose(matrix, matrix.T, atol=1e-8):
                     matrix = 0.5 * (matrix + matrix.T)