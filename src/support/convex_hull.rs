9f1feaa670f2e6a0d0906b065ab6790b363d91bf
Luca Sartore <lucasartore02@gmail.com>
2026-06-06 16:34:06 +0200

    add random sampling in convex hull

--- src/support/convex_hull.rs
+++ src/support/convex_hull.rs
@@ -1,16 +1,28 @@
-use crate::{evolution::Cost, models::Point};
+use crate::{evolution::{Cost, Random}, models::Point};
 
 #[derive(Clone, Debug)]
 pub struct ConvexHull {
     pub vertexes: Vec<Point>,
+    pub triangles: Vec<[Point; 3]>,
+    pub triangles_areas: Vec<f32>,
+    pub total_area: f32,
+    pub max_z: f32, // Cached max height for the sampling step
 }
 
 impl ConvexHull {
     pub fn new(mut points: Vec<Point>) -> ConvexHull {
         if points.len() <= 2 {
-            return ConvexHull { vertexes: points };
+            let max_z = points.iter().map(|p| p.z).fold(0.0, f32::max);
+            return ConvexHull { 
+                vertexes: points,
+                triangles: Vec::new(),
+                triangles_areas: Vec::new(),
+                total_area: 0.0,
+                max_z,
+            };
         }
 
+        // --- Your Graham Scan implementation (Calculated in XY plane) ---
         let (start_point_index, start_point) = points
             .iter()
             .copied()
@@ -31,12 +43,11 @@ impl ConvexHull {
         points.sort_by_key(|x| {
             let diff = *x - start_point;
             let angle = f32::atan2(diff.y, diff.x);
-            let distance = diff.abs();
+            let distance = (diff.x * diff.x + diff.y * diff.y).sqrt();
             (Cost::new(-angle), Cost::new(distance))
         });
 
         let mut hull = vec![start_point];
-
         for &point in &points {
             while hull.len() > 1 {
                 let len = hull.len();
@@ -51,25 +62,52 @@ impl ConvexHull {
             }
             hull.push(point);
         }
+        // -----------------------------------------------------------------
 
-        ConvexHull { vertexes: hull }
+        // Track max height from the final hull vertices
+        let mut max_z = 0.0_f32;
+        let mut triangles = Vec::new();
+        let mut triangles_areas = Vec::new();
+        let mut total_area = 0.;
+
+        if hull.len() >= 3 {
+            let p0 = hull[0];
+            max_z = max_z.max(p0.z);
+
+            for i in 1..(hull.len() - 1) {
+                let p1 = hull[i];
+                let p2 = hull[i + 1];
+                
+                max_z = max_z.max(p1.z).max(p2.z);
+
+                // Area calculations ignore Z completely
+                let v1 = p1 - p0;
+                let v2 = p2 - p0;
+                let tri_area = (Point::cross_2d(v1, v2)).abs() * 0.5;
+
+                if tri_area > 0.0 {
+                    triangles.push([p0, p1, p2]);
+                    triangles_areas.push(tri_area);
+                    total_area += tri_area;
+                }
+            }
+        } else {
+            max_z = hull.iter().map(|p| p.z).fold(0.0, f32::max);
+        }
+
+        ConvexHull { 
+            vertexes: hull, 
+            triangles, 
+            triangles_areas,
+            total_area,
+            max_z,
+        }
     }
 
     pub fn area(&self) -> f32 {
-        let points = &self.vertexes;
-
-        if points.len() < 3 {
-            return 0.0;
-        }
-
-        let mut area = 0.0;
-        for i in 0..points.len() {
-            let j = (i + 1) % points.len();
-            area += points[i].x * points[j].y;
-            area -= points[j].x * points[i].y;
-        }
-
-        area.abs() / 2.0
+        // Since we compute and cache the area during construction, 
+        // we can return it instantly here!
+        self.total_area
     }
 
     pub fn perimeter(&self) -> f32 {
@@ -89,349 +127,40 @@ impl ConvexHull {
 
         perimeter
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    pub fn random_point(&self, rand: &Random) -> Point {
+        if self.triangles.is_empty() || self.total_area <= 0.0 {
+            return Point::ZERO
+        }
+        let random_z = rand.next_f32(0.0, self.max_z);
 
-    #[test]
-    fn test_triangle_convex_hull() {
-        let points = vec![
-            Point {
-                x: 0.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 4.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 2.0,
-                y: 3.0,
-                z: 0.0,
-            },
-        ];
-        let hull = ConvexHull::new(points);
-        assert_eq!(hull.vertexes.len(), 3);
-    }
+        if self.triangles.is_empty() {
+            let mut p = Point::random_in_between(
+                self.random_point(rand),
+                self.random_point(rand),
+                rand
+            );
+            p.z = random_z;
+            return p;
+        }
 
-    #[test]
-    fn test_square_convex_hull() {
-        let points = vec![
-            Point {
-                x: 0.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 1.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 1.0,
-                y: 1.0,
-                z: 0.0,
-            },
-            Point {
-                x: 0.0,
-                y: 1.0,
-                z: 0.0,
-            },
-            Point {
-                x: 0.5,
-                y: 0.5,
-                z: 0.0,
-            }, // Interior point
-        ];
-        let hull = ConvexHull::new(points);
-        assert_eq!(hull.vertexes.len(), 4);
-    }
+        let t = rand.choose_weighted(&self.triangles, &self.triangles_areas)
+            .expect("triangle selection can't fail");
 
-    #[test]
-    fn test_convex_hull_ignores_z() {
-        let points = vec![
-            Point {
-                x: 0.0,
-                y: 0.0,
-                z: 100.0,
-            },
-            Point {
-                x: 1.0,
-                y: 0.0,
-                z: -50.0,
-            },
-            Point {
-                x: 0.5,
-                y: 1.0,
-                z: 0.0,
-            },
-        ];
-        let hull = ConvexHull::new(points);
-        assert_eq!(hull.vertexes.len(), 3);
-    }
+        let r1 = rand.next_f32(0.0, 1.0);
+        let r2 = rand.next_f32(0.0, 1.0);
+        
+        let sqrt_r1 = r1.sqrt();
+        let w_a = 1.0 - sqrt_r1;
+        let w_b = sqrt_r1 * (1.0 - r2);
+        let w_c = sqrt_r1 * r2;
 
-    #[test]
-    fn test_single_point_hull() {
-        let points = vec![Point {
-            x: 5.0,
-            y: 5.0,
-            z: 0.0,
-        }];
-        let hull = ConvexHull::new(points);
-        assert_eq!(hull.vertexes.len(), 1);
-    }
 
-    #[test]
-    fn test_two_points_hull() {
-        let points = vec![
-            Point {
-                x: 0.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 1.0,
-                y: 1.0,
-                z: 0.0,
-            },
-        ];
-        let hull = ConvexHull::new(points);
-        assert_eq!(hull.vertexes.len(), 2);
-    }
-
-    #[test]
-    fn test_triangle_area() {
-        let points = vec![
-            Point {
-                x: 0.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 4.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 0.0,
-                y: 3.0,
-                z: 0.0,
-            },
-        ];
-        let hull = ConvexHull::new(points);
-        let area = hull.area();
-        assert!((area - 6.0).abs() < 0.0001);
-    }
-
-    #[test]
-    fn test_square_area() {
-        let points = vec![
-            Point {
-                x: 0.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 2.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 2.0,
-                y: 2.0,
-                z: 0.0,
-            },
-            Point {
-                x: 0.0,
-                y: 2.0,
-                z: 0.0,
-            },
-            Point {
-                x: 1.0,
-                y: 1.0,
-                z: 1.0,
-            },
-            Point {
-                x: 1.1,
-                y: 0.3,
-                z: 1.0,
-            },
-        ];
-        let hull = ConvexHull::new(points);
-        let area = hull.area();
-        assert!((area - 4.0).abs() < 0.0001);
-    }
-
-    #[test]
-    fn test_empty_shape_area() {
-        let shape = ConvexHull { vertexes: vec![] };
-        let area = shape.area();
-        assert_eq!(area, 0.0);
-    }
-
-    #[test]
-    fn test_single_point_area() {
-        let shape = ConvexHull {
-            vertexes: vec![Point {
-                x: 1.0,
-                y: 1.0,
-                z: 0.0,
-            }],
-        };
-        let area = shape.area();
-        assert_eq!(area, 0.0);
-    }
-
-    #[test]
-    fn test_two_point_area() {
-        let shape = ConvexHull {
-            vertexes: vec![
-                Point {
-                    x: 0.0,
-                    y: 0.0,
-                    z: 0.0,
-                },
-                Point {
-                    x: 1.0,
-                    y: 1.0,
-                    z: 0.0,
-                },
-            ],
-        };
-        let area = shape.area();
-        assert_eq!(area, 0.0);
-    }
-
-    #[test]
-    fn test_pentagon_area() {
-        let points = vec![
-            Point {
-                x: 1.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 0.309,
-                y: 0.951,
-                z: 0.0,
-            },
-            Point {
-                x: -0.809,
-                y: 0.588,
-                z: 0.0,
-            },
-            Point {
-                x: -0.809,
-                y: -0.588,
-                z: 0.0,
-            },
-            Point {
-                x: 0.309,
-                y: -0.951,
-                z: 0.0,
-            },
-        ];
-        let hull = ConvexHull::new(points);
-        let area = hull.area();
-        assert!(area > 0.0 && area < 3.0);
-    }
-
-    #[test]
-    fn test_square_perimeter() {
-        let points = vec![
-            Point {
-                x: 0.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 1.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 1.0,
-                y: 1.0,
-                z: 0.0,
-            },
-            Point {
-                x: 0.0,
-                y: 1.0,
-                z: 0.0,
-            },
-        ];
-        let hull = ConvexHull::new(points);
-        let perimeter = hull.perimeter();
-        assert!((perimeter - 4.0).abs() < 0.0001);
-    }
-
-    #[test]
-    fn test_triangle_perimeter() {
-        let points = vec![
-            Point {
-                x: 0.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 3.0,
-                y: 0.0,
-                z: 0.0,
-            },
-            Point {
-                x: 0.0,
-                y: 4.0,
-                z: 0.0,
-            },
-        ];
-        let hull = ConvexHull::new(points);
-        let perimeter = hull.perimeter();
-        // 3 + 4 + 5 = 12
-        assert!((perimeter - 12.0).abs() < 0.0001);
-    }
-
-    #[test]
-    fn test_empty_perimeter() {
-        let shape = ConvexHull { vertexes: vec![] };
-        let perimeter = shape.perimeter();
-        assert_eq!(perimeter, 0.0);
-    }
-
-    #[test]
-    fn test_single_point_perimeter() {
-        let shape = ConvexHull {
-            vertexes: vec![Point {
-                x: 1.0,
-                y: 1.0,
-                z: 0.0,
-            }],
-        };
-        let perimeter = shape.perimeter();
-        assert_eq!(perimeter, 0.0);
-    }
-
-    #[test]
-    fn test_two_point_perimeter() {
-        let shape = ConvexHull {
-            vertexes: vec![
-                Point {
-                    x: 0.0,
-                    y: 0.0,
-                    z: 0.0,
-                },
-                Point {
-                    x: 3.0,
-                    y: 4.0,
-                    z: 0.0,
-                },
-            ],
-        };
-        let perimeter = shape.perimeter();
-        // Distance from (0,0) to (3,4) is 5, so perimeter is 5 + 5 = 10
-        assert!((perimeter - 10.0).abs() < 0.0001);
+        Point {
+            x: w_a * t[0].x + w_b * t[1].x + w_c * t[2].x,
+            y: w_a * t[0].y + w_b * t[1].y + w_c * t[2].y,
+            z: random_z,
+        }
     }
 }
+
